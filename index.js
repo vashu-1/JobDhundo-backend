5e39f0ef86aa10f6776a4f02c64ca147b5aebc5a
Vijay Kumar Modak <vijaymodak9945@gmail.com>
2025-07-12 19:34:56 +0530

    adding backend on git

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+
+import connectDB from "./utils/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "Welcome to JobDhundo Backend",
+    success: true,
+  });
+});
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+  origin: "http://localhost:5173", // Replace with your frontend URL
+  credentials: true, // Allow cookies to be sent
+};
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+//api's
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
